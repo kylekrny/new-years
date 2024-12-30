47e7bf54f55b6b688d54545bcaa358f3eb0dc613
Kyle <45010713+kylekrny@users.noreply.github.com>
2024-12-30 15:08:34 -0500

    built basic header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import Header from './components/Header';
 import List from './components/List';
 
 function App() {
   return (
-    <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
+    <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 mt-12'>
+      <Header />
       <List />
     </div>
   );
