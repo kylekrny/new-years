3d72ce918db2f92d1144dcbcab0afe799c232192
Kyle <45010713+kylekrny@users.noreply.github.com>
2024-12-31 21:49:59 -0500

    clean up modal button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,4 @@
-import { PlusIcon } from '@heroicons/react/20/solid';
-import { useContext } from 'react';
-import { AppContext } from './context';
-
 const Header = () => {
-  const context = useContext(AppContext);
   return (
     <div className='md:flex md:items-center md:justify-between mb-12'>
       <div className='min-w-0 flex-1'>
@@ -17,16 +12,6 @@ const Header = () => {
           space.
         </p>
       </div>
-      {/* <div className='mt-4 flex md:ml-4 md:mt-0'>
-        <button
-          type='button'
-          className='inline-flex items-center gap-x-1.5 rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm ring-2 ring-inset ring-black hover:bg-gray-50'
-          onClick={() => context?.setOpen(true)}
-        >
-          Add Resolution
-          <PlusIcon aria-hidden='true' className='size-5' />
-        </button>
-      </div> */}
     </div>
   );
 };
