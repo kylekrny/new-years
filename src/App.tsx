f07234221798e35a6f0f6c8e350413dc4e86a3d7
Kyle <45010713+kylekrny@users.noreply.github.com>
2024-12-31 21:25:41 -0500

    remove setSubmitted/submitted context

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,10 @@ import { AppContext } from './components/context';
 import { Resolution } from './firestore';
 
 function App() {
-  const [submitted, setSubmitted] = useState<boolean>(false);
   const [resolutions, setResolutions] = useState<Resolution[]>([]);
 
   return (
-    <AppContext.Provider
-      value={{ submitted, setSubmitted, resolutions, setResolutions }}
-    >
+    <AppContext.Provider value={{ resolutions, setResolutions }}>
       <div className='mx-auto max-w-4xl px-4 sm:px-8 lg:px-10 py-8 mt-12'>
         <Header />
         <List />
--- src/components/context.tsx
+++ src/components/context.tsx
@@ -2,8 +2,6 @@ import { createContext, SetStateAction, Dispatch } from 'react';
 import { Resolution } from '../firestore';
 
 type AppContextType = {
-  submitted: boolean;
-  setSubmitted: Dispatch<SetStateAction<boolean>>;
   resolutions: Array<Resolution>;
   setResolutions: Dispatch<SetStateAction<Resolution[]>>;
 };
